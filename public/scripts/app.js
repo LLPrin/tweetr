4e5fe2ccc6a345ca6daa21feaf25a4f1fa85e820
LLPrin <LLPrin@users.noreply.github.com>
2016-10-21 21:01:13 +0000

    completed compose button toggle

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,58 +4,114 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+
+$(function() {
+  console.log('im ready');
+  function renderTweets(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container\
+    var rawTweetData = '';
+    for (let user of tweets ) {
+      rawTweetData = createTweetElement(user) + rawTweetData;
+    }
+    //for form vaildation change from append to prepend
+    return  $('#oldTweets').prepend(rawTweetData);
+
+  }
+
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+  function createTweetElement(tweet) {
+
+    return `
+      <article class="tweet">
+            <header>
+              <img class="logo" src="${tweet.user.avatars.small}">
+              <span class="username">${tweet.user.name}</span>
+              <span class="handle">${tweet.user.handle}</span>
+            </header>
+            <p class="tweettext">${escape(tweet.content.text)}</p>
+            <footer class="timestamp">
+              ${tweet.created_at}
+              <span class="glyphicon glyphicon-heart-empty"></span>
+              <span class="glyphicon glyphicon-refresh"></span>
+              <span class="glyphicon glyphicon-flag"></span>
+            </footer>
+      </article>
+      `;
+  }
 
 
+  //---------------------AJAX SUBMIT TWEET
 
- $(function(){
+  $(':input:submit').on('click', function(event) {
+    event.preventDefault();
+    let data = $("[name='text']").serialize();
+      if (data.length > 140) {
+        alert('Your tweet must be below 140 characters.')
+      } else {
+        $.ajax({
+          url: '/tweets',
+          method: 'POST',
+          data: $( "[name='text']" ).serialize(),
+          }).then(function successCb(data) {
+            // console.log('data from server resp', data);
+              window.location.reload(true);
+            }, function errorCb(err) {
+              alert('Cannot post empty text.')
+              console.error(err);
+            });
+      }
+  });
 
+  function loadTweets(){
+    // console.log('ive been called!');
+    $.ajax({
+      //handling JSON response instead.
+      url: '/tweets',
+      method: 'GET'
+    }).then(function successCb(data) {
+      // console.log('data from server resp', data);
+      renderTweets(data);
+      }, function errorCb(err) {
+      console.error(err);
+    });
+  }
 
-  // $('section#oldTweets').on('hover',
-  // // function () {
-  // //   $('.glyphicon').css('opacity', 0);
-  // // },
-  // function () {
-  //   $('span.glyphicon').css('opacity', '100');
-  // }
-  // );
+   loadTweets();
 
-// var oldweets = document.createElement()
+//-------------Button Slide
+  $('#compose').on('click', function() {
+    event.preventDefault();
+    if(!$(this).hasClass('.active')) {
+      $('#compose').find('.active').removeClass('.active');
+      $(this).addClass('.active');
+      $('.new-tweet').slideUp();
+      console.log("slides here")
+      $('.new-tweet[id=' + $(this).attr('data-id') + ']').slideDown();
 
-//   var $tweet = $("<article>").addClass("tweet");
-
- // $('textarea').on('keyup', function(event){
- //  // function updateCounter()
- //        var Characters = 140 - $(this).val().length;
- //        $('.counter').text(Characters);
-
- //        if(Characters < 0){
- //          $('.counter').css('color', 'red');
- //        }
-
- // });
-
+    } else {
+      $('.new-tweet').slideDown();
+      $('textarea').focus();
+      $('#compose').removeClass('.active');
+      }
+  })
 
 });
 
-//  // Test / driver code (temporary). Eventually will get this from the server.
-// var tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": {
-//       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//     },
-//     "handle": "@SirIsaac"
-//   },
-//   "content": {
-//     "text": "If I have seen further it is by standing on the shoulders of giants"
-//   },
-//   "created_at": 1461116232227
-// }
+
 
 // var $tweet = createTweetElement(tweetData);
 
+
+
 // // Test / driver code (temporary)
 // console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+// $('#oldTweets').append($tweet);
+
